c7a45793f89be8a0a27bd187b4f47eb865e9a30b
Namit Pasrija <namit.pasrija@gmail.com>
2018-01-11 23:48:26 +0530

    edit corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  before_action :authenticate_user!,only:[:editprofile]
   def profile
   	@user=User.find_by_id(params[:userid])
     @attempts=Attempt.where(user_id: params[:userid])
